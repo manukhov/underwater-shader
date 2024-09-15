082a6d8d5e800d995d13131aeb76bf95384dc27e
Владимир Манухов <vmanukhov@vk.com>
2024-09-15 07:16:41 +0300

    Updated

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include "injector\hooking.hpp"
 #include "RenderWare.h"
 
-#include <d3dx9.h>
 #include <fstream>
 #include <sstream>
+
+#include <d3dx9.h>
 #pragma comment(lib, "d3dx9.lib")
 
 LPD3DXBUFFER pCode;
 LPD3DXBUFFER pErrors;
 
-DWORD g_Shader = NULL;
-RwRaster* g_Raster = NULL;
+IDirect3DPixelShader9* g_Shader;
+RwRaster* g_Raster = nullptr;
 
 void (*fnrender)();
 
@@ -29,11 +30,11 @@ public:
 };
 
 #define GameCamera (*(RwCamera **)0xC1703C)
-#define RwRenderStateSet(state, value) (((void (*)(RwRenderState, unsigned int))0x7FE420)(state, value))
-#define RwD3D9SetPixelShader(shader) (((void (*)(DWORD))0x7F9FF0)(shader))
+#define RwRenderStateSet(state, value) (((void (__cdecl *)(RwRenderState, unsigned int))0x7FC2D0)(state, value))
+#define RwD3D9SetPixelShader(shader) (((void (__cdecl *)(IDirect3DPixelShader9*))0x7F9FF0)(shader))
 #define RwD3D9SetPixelShaderConstantF(StartRegister, pConstantData, Vector4fCount) (((HRESULT (__cdecl *)(UINT, const float *, UINT))0x7FAD00)(StartRegister, pConstantData, Vector4fCount))
-#define RwD3D9CreatePixelShader(pfunction, shader) (((bool (*)(DWORD *, DWORD *))0x7FACC0)(pfunction, shader))
-#define RwD3D9DeletePixelShader(shader) (((void (*)(DWORD))0x7FACF0)(shader))
+#define RwD3D9CreatePixelShader(pfunction, shader) (((bool(__cdecl *)(DWORD*, IDirect3DPixelShader9**))0x7FACC0)(pfunction, shader))
+#define RwD3D9DeletePixelShader(shader) (((void (*)(IDirect3DPixelShader9*))0x7FACF0)(shader))
 #define RwRasterCreate(width, height, depth, flags) ((RwRaster *(__cdecl *)(int, int, int, int))0x7FB230)(width, height, depth, flags)
 #define RwRasterDestroy(raster) ((void (__cdecl *)(RwRaster *))0x7FB020)(raster)
 #define RwRasterRenderFast(raster, x, y) ((RwRaster * (__cdecl *)(RwRaster *, int, int))0x7FAF50)(raster, x, y)
@@ -44,12 +45,17 @@ public:
 #define CSprite2d__Draw(sprite, rect, color) (((void (__thiscall *)(void *, CRect&, CRGBA&))0x728350)(sprite, rect, color))
 
 const char* shaderHLSL =
-"float4 main(float2 texCoord : TEXCOORD) : COLOR {\n"
-"    return float4(0.0f, 0.0f, 1.0f, 1.0f); \n"
+"sampler2D tex0 : register(s0);\n"
+"float4 c0 = float4(0.30f, 0.59f, 0.11f, 1.0f);\n"
+"\n"
+"float4 main(float2 texCoord : TEXCOORD0) : SV_Target {\n"
+"    float4 texColor = tex2D(tex0, texCoord); \n"
+"    float gray = dot(texColor.rgb, c0.rgb); \n"
+"    return float4(gray, gray, gray, c0.a); \n"
 "}\n"
-"technique BlueScreen {\n"
+"technique GrayscaleScreen {\n"
 "    pass P0 {\n"
-"        PixelShader = compile ps_3_0 main();\n"
+"        PixelShader = compile ps_3_0 mainPS();\n"
 "    }\n"
 "}\n";
 
@@ -97,22 +103,9 @@ void CompileShader()
     }
 }
 
-
-void SetShaderCB(DWORD shader)
+void SetShaderCB()
 {
     RwD3D9SetPixelShader(g_Shader);
-
-    float distortionIntensity[2] = { 0.05f, 0.05f };
-
-    HRESULT result = RwD3D9SetPixelShaderConstantF(0, distortionIntensity, 1);
-    if (SUCCEEDED(result))
-    {
-        LogMessage("Pixel shader constant set successfully.");
-    }
-    else
-    {
-        LogMessage("Failed to set pixel shader constant.");
-    }
 }
 
 void Render()
@@ -131,7 +124,7 @@ void Render()
         {
             LogMessage("Destroying old raster.");
             RwRasterDestroy(g_Raster);
-            g_Raster = NULL;
+            g_Raster = nullptr;
         }
 
         if (!g_Raster)
@@ -139,22 +132,20 @@ void Render()
             LogMessage("Creating new raster.");
             g_Raster = RwRasterCreate(width, height, depth, rwRASTERTYPECAMERATEXTURE);
         }
-
         RwCameraEndUpdate(GameCamera);
         RwRasterPushContext(g_Raster);
         RwRasterRenderFast(GameCamera->frameBuffer, 0, 0);
         RwRasterPopContext();
         RwCameraBeginUpdate(GameCamera);
     }
-    SetShaderCB(g_Shader);
+    injector::MakeCALL(0x7FB824, SetShaderCB);
     RwRenderStateSet(rwRENDERSTATEVERTEXALPHAENABLE, FALSE);
     RwRenderStateSet(rwRENDERSTATESRCBLEND, 5); // D3DBLEND_SRCALPHA
     RwRenderStateSet(rwRENDERSTATEDESTBLEND, 6); // D3DBLEND_INVSRCALPHA
-    void* pRaster = &g_Raster;
+    void *pRaster = &g_Raster;
     CSprite2d__Draw(&pRaster, CRect(1.0f, 0.5f, g_Raster->width + 1.0f, g_Raster->height + 0.5f), CRGBA(255, 255, 255, 255));
     injector::MakeCALL(0x7FB824, 0x7F9FF0);
 }
-
 
 
 BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID reserved)
