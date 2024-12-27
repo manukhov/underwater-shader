15c2cb44cbfdbea858d67fc25fe11d54d7d02477
Владимир Манухов <vmanukhov@vk.com>
2024-12-27 05:41:59 +0300

    Updated

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
-#include "injector\hooking.hpp"
-#include "RenderWare.h"
+#pragma once
 
-#include <fstream>
-#include <sstream>
+#include "plugin.h"
+#include "RenderWare.h"
+#include "CSprite2d.h"
 
 #include <d3dx9.h>
 #pragma comment(lib, "d3dx9.lib")
@@ -10,154 +10,201 @@
 LPD3DXBUFFER pCode;
 LPD3DXBUFFER pErrors;
 
-IDirect3DPixelShader9* g_Shader;
+IDirect3DDevice9* pDevice = nullptr;
+ID3DXBuffer* pDisassembly = nullptr;
+
+IDirect3DPixelShader9* g_Shader = nullptr;
+IDirect3DPixelShader9* g_CurrentShader = nullptr;
+
 RwRaster* g_Raster = nullptr;
+
+bool bShaderCompiled = false;
+bool bShaderSetSuccessfully = false;
+bool bFirstRun = true;
 
 void (*fnrender)();
 
-class CRGBA
-{
-    unsigned char r, g, b, a;
-public:
-    CRGBA(unsigned char _1, unsigned char _2, unsigned char _3, unsigned char _4) : r(_1), g(_2), b(_3), a(_4) {}
-};
+CSprite2d Sprite2d;
 
-class CRect
-{
-    float left, bottom, right, top;
-public:
-    CRect(float _1, float _2, float _3, float _4) : left(_1), top(_2), right(_3), bottom(_4) {}
-};
+#define GameCamera (*(RwCamera**)0xC1703C)
 
-#define GameCamera (*(RwCamera **)0xC1703C)
-#define RwRenderStateSet(state, value) (((void (__cdecl *)(RwRenderState, unsigned int))0x7FC2D0)(state, value))
-#define RwD3D9SetPixelShader(shader) (((void (__cdecl *)(IDirect3DPixelShader9*))0x7F9FF0)(shader))
-#define RwD3D9SetPixelShaderConstantF(StartRegister, pConstantData, Vector4fCount) (((HRESULT (__cdecl *)(UINT, const float *, UINT))0x7FAD00)(StartRegister, pConstantData, Vector4fCount))
-#define RwD3D9CreatePixelShader(pfunction, shader) (((bool(__cdecl *)(DWORD*, IDirect3DPixelShader9**))0x7FACC0)(pfunction, shader))
-#define RwD3D9DeletePixelShader(shader) (((void (*)(IDirect3DPixelShader9*))0x7FACF0)(shader))
-#define RwRasterCreate(width, height, depth, flags) ((RwRaster *(__cdecl *)(int, int, int, int))0x7FB230)(width, height, depth, flags)
-#define RwRasterDestroy(raster) ((void (__cdecl *)(RwRaster *))0x7FB020)(raster)
-#define RwRasterRenderFast(raster, x, y) ((RwRaster * (__cdecl *)(RwRaster *, int, int))0x7FAF50)(raster, x, y)
-#define RwRasterPushContext(raster) ((RwRaster * (__cdecl *)(RwRaster *))0x7FB060)(raster)
-#define RwRasterPopContext() ((RwRaster * (__cdecl *)())0x7FB110)()
-#define RwCameraEndUpdate(camera) ((RwCamera *(__cdecl *)(RwCamera *))0x7EE180)(camera)
-#define RwCameraBeginUpdate(camera) ((RwCamera *(__cdecl *)(RwCamera *))0x7EE190)(camera)
-#define CSprite2d__Draw(sprite, rect, color) (((void (__thiscall *)(void *, CRect&, CRGBA&))0x728350)(sprite, rect, color))
-
-const char* shaderHLSL =
-"sampler2D tex0 : register(s0);\n"
-"float4 c0 = float4(0.30f, 0.59f, 0.11f, 1.0f);\n"
+const char* HLSL =
+"sampler2D tex0 : register(s0); \n"
+"float4 c0 = float4(0.30f, 0.59f, 0.11f, 1.0f); \n"
 "\n"
-"float4 main(float2 texCoord : TEXCOORD0) : SV_Target {\n"
+"float4 main(float2 texCoord : TEXCOORD0) : COLOR \n"
+"{ \n"
 "    float4 texColor = tex2D(tex0, texCoord); \n"
 "    float gray = dot(texColor.rgb, c0.rgb); \n"
-"    return float4(gray, gray, gray, c0.a); \n"
-"}\n"
-"technique GrayscaleScreen {\n"
-"    pass P0 {\n"
-"        PixelShader = compile ps_3_0 main();\n"
-"    }\n"
-"}\n";
+"    return float4(gray, gray, gray, texColor.a); \n"
+"} \n"
+"\n"
+"technique GrayscaleScreen \n"
+"{ \n"
+"    pass P0 \n"
+"    { \n"
+"        PixelShader = compile ps_3_0 main(); \n"
+"    } \n"
+"} \n";
 
 
-void LogMessage(const char* message)
+void Debug(const char* message)
 {
+    if (bFirstRun)
+    {
+        std::remove("debug.txt");
+        bFirstRun = false;
+    }
     std::ofstream logFile("debug.txt", std::ios_base::app);
     logFile << message << std::endl;
 }
 
 void CompileShader()
 {
-    LogMessage("Starting shader compilation...");
+    HRESULT CompileResult = D3DXCompileShader(HLSL, strlen(HLSL), NULL, NULL, "main", "ps_3_0", 0, &pCode, &pErrors, NULL);
 
-    HRESULT result = D3DXCompileShader(shaderHLSL, strlen(shaderHLSL), NULL, NULL, "main", "ps_3_0", 0, &pCode, &pErrors, NULL);
-
-    if (FAILED(result))
+    if (FAILED(CompileResult))
     {
-        LogMessage("Shader compilation failed.");
+        Debug("Shader compilation failed");
+
         if (pErrors)
         {
             std::string errorMsg = "Shader Compilation Error: ";
             errorMsg += static_cast<char*>(pErrors->GetBufferPointer());
-            LogMessage(errorMsg.c_str());
+            Debug(errorMsg.c_str());
             pErrors->Release();
         }
     }
     else
     {
-        LogMessage("Shader compilation succeeded.");
+        Debug("Shader compilation succeeded");
 
         if (pCode)
         {
-            std::string shaderCodeSizeMsg = "Shader code size: ";
+            std::string shaderCodeSizeMsg = "Code size: ";
             shaderCodeSizeMsg += std::to_string(pCode->GetBufferSize());
-            LogMessage(shaderCodeSizeMsg.c_str());
+            Debug(shaderCodeSizeMsg.c_str());
         }
-
-        if (!RwD3D9CreatePixelShader((DWORD*)pCode->GetBufferPointer(), &g_Shader))
-            LogMessage("Shader creation failed.");
+        if (!RwD3D9CreatePixelShader((RwUInt32*)pCode->GetBufferPointer(), &g_Shader))
+            Debug("Shader creation failed");
         else
-            LogMessage("Shader creation succeeded.");
+        {
+            Debug("Shader creation succeeded");
 
+            HRESULT DisasmResult = D3DXDisassembleShader((DWORD*)pCode->GetBufferPointer(), FALSE, nullptr, &pDisassembly);
+            if (SUCCEEDED(DisasmResult))
+            {
+                if (pDisassembly && pDisassembly->GetBufferSize() > 0)
+                {
+                    const char* shaderText = (const char*)pDisassembly->GetBufferPointer();
+                    Debug(shaderText);
+                }
+                else
+                    Debug("D3DXDisassembleShader: Buffer is empty");
+            }
+            else
+                Debug("D3DXDisassembleShader failed");
+        }
+        bShaderCompiled = true;
+        pDisassembly->Release();
         pCode->Release();
     }
 }
 
 void SetShaderCB()
 {
-    RwD3D9SetPixelShader(g_Shader);
-    IDirect3DDevice9* pDevice = *(IDirect3DDevice9**)(0xC97C28);
+    _rwD3D9SetPixelShader(g_Shader);
 
-    IDirect3DPixelShader9* CurrentShader = nullptr;
-    pDevice->GetPixelShader(&CurrentShader);
+    pDevice = *(IDirect3DDevice9**)(0xC97C28);
 
-    if (CurrentShader == g_Shader)
-        LogMessage("Pixel shader set successfully.");
-    else
-        LogMessage("Failed to set pixel shader.");
+    if (!bShaderSetSuccessfully)
+    {
+        g_CurrentShader = nullptr;
 
-    if (CurrentShader)
-        CurrentShader->Release();
+        pDevice->GetPixelShader(&g_CurrentShader);
+
+        if (g_CurrentShader == g_Shader)
+        {
+            Debug("Pixel shader set successfully");
+            bShaderSetSuccessfully = true;
+        }
+        else
+            Debug("Failed to set pixel shader");
+
+        if (g_CurrentShader)
+            g_CurrentShader->Release();
+    }
 }
 
 void Render()
 {
     fnrender();
 
-    CompileShader();
+    if (!bShaderCompiled)
+        CompileShader();
 
-    if (GameCamera->frameBuffer)
+    if (GameCamera && GameCamera->frameBuffer)
     {
-        auto width = GameCamera->frameBuffer->width;
-        auto height = GameCamera->frameBuffer->height;
-        auto depth = GameCamera->frameBuffer->depth;
+        RwUInt32 width = GameCamera->frameBuffer->width;
+        RwUInt32 height = GameCamera->frameBuffer->height;
+        RwUInt32 depth = GameCamera->frameBuffer->depth;
 
-        if (g_Raster && (width != g_Raster->width || height != g_Raster->height || depth != g_Raster->depth))
+        if (workBuffer && (width != workBuffer->width || height != workBuffer->height || depth != workBuffer->depth))
         {
-            LogMessage("Destroying old raster.");
-            RwRasterDestroy(g_Raster);
-            g_Raster = nullptr;
+            Debug("Destroying old raster");
+            RwRasterDestroy(workBuffer);
+            workBuffer = NULL;
         }
-
-        if (!g_Raster)
+        if (workBuffer == NULL)
         {
-            LogMessage("Creating new raster.");
-            g_Raster = RwRasterCreate(width, height, depth, rwRASTERTYPECAMERATEXTURE);
+            workBuffer = RwRasterCreate(pRasterFrontBuffer->width, pRasterFrontBuffer->height, pRasterFrontBuffer->depth, rwRASTERTYPECAMERATEXTURE);
+            if (workBuffer == NULL)
+            {
+                Debug("Failed to create raster");
+                return;
+            }
+            Debug("Raster created successfully");
         }
-        RwCameraEndUpdate(GameCamera);
-        RwRasterPushContext(g_Raster);
-        RwRasterRenderFast(GameCamera->frameBuffer, 0, 0);
-        RwRasterPopContext();
-        RwCameraBeginUpdate(GameCamera);
     }
+    else
+    {
+        Debug("Game camera or frame buffer is invalid");
+        return;
+    }
+
+    RwCameraEndUpdate(GameCamera);
+    RwRasterPushContext(workBuffer);
+    RwRasterRenderFast(GameCamera->frameBuffer, 0, 0);
+    RwRasterPopContext();
+    RwCameraBeginUpdate(GameCamera);
+
     injector::MakeCALL(0x7FB824, SetShaderCB);
-    RwRenderStateSet(rwRENDERSTATEVERTEXALPHAENABLE, FALSE);
-    RwRenderStateSet(rwRENDERSTATESRCBLEND, 5); // D3DBLEND_SRCALPHA
-    RwRenderStateSet(rwRENDERSTATEDESTBLEND, 6); // D3DBLEND_INVSRCALPHA
-    void *pRaster = &g_Raster;
-    CSprite2d__Draw(&pRaster, CRect(1.0f, 0.5f, g_Raster->width + 1.0f, g_Raster->height + 0.5f), CRGBA(255, 255, 255, 255));
+
+
+    RwRenderStateSet(rwRENDERSTATETEXTUREFILTER, (void*)rwFILTERNEAREST);
+    RwRenderStateSet(rwRENDERSTATEFOGENABLE, (void*)FALSE);
+    RwRenderStateSet(rwRENDERSTATEZTESTENABLE, (void*)FALSE);
+    RwRenderStateSet(rwRENDERSTATEZWRITEENABLE, (void*)FALSE);
+    RwRenderStateSet(rwRENDERSTATETEXTURERASTER, (void*)workBuffer);
+    RwRenderStateSet(rwRENDERSTATEVERTEXALPHAENABLE, (void*)FALSE);
+
+
+    // Настройки для рендера квадрата
+    float x1 = 1.0f;
+    float y1 = 0.5f;
+    float x2 = workBuffer->width + 1.0f;
+    float y2 = workBuffer->height + 0.5f;
+    CRGBA color(255, 255, 255, 255);
+
+    // Отрисовка спрайта с растром
+    Sprite2d.Draw(x1, y1, x2, y2, color);
+
+    // Возвращаем настройки после отрисовки
+    RwRenderStateSet(rwRENDERSTATETEXTURERASTER, nullptr);
+
     injector::MakeCALL(0x7FB824, 0x7F9FF0);
 }
+
 
 
 BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID reserved)
